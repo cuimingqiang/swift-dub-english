9ad6bbba135b782913ce34d9949cf6c0177bb049
崔明强 <770460131@qq.com>
2016-02-14 17:59:14 +0800

    小组，tab，搜索框联系

--- dub/GroupController.swift
+++ dub/GroupController.swift
@@ -10,14 +10,19 @@ import Foundation
 import UIKit
 import SnapKit
 import MJRefresh
+import RxSwift
+import RxCocoa
 class GroupController:UIViewController{
     lazy var navigtion = UINavigationBar.init()
-    lazy var tableView = UITableView.init()
     lazy var segment = UISegmentedControl.init(items: ["查找小组","我的小组"])
+    lazy var lookTableView = UITableView.init()
+    lazy var myTableView = UITableView.init()
+    lazy var container = UIScrollView.init()
+    var lookDS = []
     override func viewDidLoad() {
         initNavi()
         initTab()
-//        initTable()
+        initContainer()
     }
 }
 
@@ -43,6 +48,7 @@ extension GroupController{
     func initTab(){
         segment.tintColor = ColorConstant.GREEN
         segment.selectedSegmentIndex = 0
+        segment.addTarget(self, action: "select", forControlEvents: UIControlEvents.ValueChanged)
         self.view.addSubview(segment)
         segment.snp_makeConstraints {
             $0.height.equalTo(30)
@@ -50,25 +56,121 @@ extension GroupController{
             $0.centerX.equalTo(view)
             $0.top.equalTo(74)
         }
+
+    }
+
+    func select(){
+        let rect = CGRectMake(CGFloat(segment.selectedSegmentIndex) * container.frame.size.width, 0,
+            container.frame.size.width, container.frame.size.height)
+        container.scrollRectToVisible(rect, animated: true)
     }
 }
 
-extension GroupController{
-    func initTable(){
-        tableView.bounces = true
-        tableView.alwaysBounceVertical = true
-        tableView.separatorStyle = UITableViewCellSeparatorStyle.None
-        tableView.sectionFooterHeight = 5
-        tableView.sectionHeaderHeight = 5
-        tableView.mj_header = MJRefreshNormalHeader.init(refreshingTarget: self, refreshingAction: "refresh")
-        self.view.addSubview(tableView)
-        tableView.snp_makeConstraints {
-            $0.top.equalTo(64)
+extension GroupController : UIScrollViewDelegate{
+    func initContainer(){
+        container.pagingEnabled = true
+        container.delegate = self
+        container.showsHorizontalScrollIndicator = false
+        container.showsVerticalScrollIndicator = false
+        self.view.addSubview(container)
+        container.snp_makeConstraints {
+            $0.top.equalTo(114)
             $0.width.equalTo(view)
             $0.bottom.equalTo(view)
         }
+        container.contentSize = CGSizeMake(UIScreen.mainScreen().bounds.width * 2, 553)
+        initLookTableView(container)
+        initMyTableView(container)
+    }
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let page = Int(container.contentOffset.x * 2 / container.frame.width)
+        segment.selectedSegmentIndex = page
+    }
+}
+
+extension GroupController : UITableViewDelegate,UITableViewDataSource ,UISearchBarDelegate{
+    func initLookTableView(parentView:UIView){
+        lookTableView.delegate = self
+        lookTableView.dataSource = self
+        lookTableView.bounces = true
+        lookTableView.alwaysBounceVertical = true
+        lookTableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        lookTableView.sectionFooterHeight = 5
+        lookTableView.sectionHeaderHeight = 5
+        lookTableView.mj_header = MJRefreshNormalHeader.init(refreshingTarget: self, refreshingAction: "refresh")
+        parentView.addSubview(lookTableView)
+        lookTableView.snp_makeConstraints {
+            $0.width.height.top.equalTo(parentView)
+        }
     }
     func refresh(){
-        tableView.mj_header.endRefreshing()
+        lookTableView.mj_header.endRefreshing()
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell : UITableViewCell!
+        switch indexPath.section{
+        case 0:
+            cell = UITableViewCell.init()
+            cell.selectionStyle = UITableViewCellSelectionStyle.None
+            let search = UISearchBar.init()
+            search.backgroundColor = UIColor.blackColor()
+            search.placeholder = "搜索小组号或小组名"
+            search.delegate = self
+            cell.addSubview(search)
+            search.snp_makeConstraints{
+                $0.width.height.equalTo(cell)
+            }
+        case 1:
+            cell = UITableViewCell.init()
+        default:
+            cell = UITableViewCell.init()
+        }
+        return cell
+    }
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        print("searchBarShouldBeginEditing")
+        return false
+    }
+
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        switch indexPath.section{
+        case 0:
+            return 40
+        case 1:
+            return 44
+        default:
+            return 44
+        }
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section{
+        case 0...1:
+            return 1
+        default:
+            return lookDS.count
+        }
+    }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
+    }
+
+}
+extension GroupController{
+    func initMyTableView(parentView : UIView){
+        myTableView.bounces = true
+        myTableView.alwaysBounceVertical = true
+        myTableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        myTableView.sectionFooterHeight = 5
+        myTableView.sectionHeaderHeight = 5
+        myTableView.mj_header = MJRefreshNormalHeader.init(refreshingTarget: self, refreshingAction: "refresh_my")
+        parentView.addSubview(myTableView)
+        myTableView.snp_makeConstraints {
+            $0.width.height.top.equalTo(parentView)
+            $0.left.equalTo(UIScreen.mainScreen().bounds.width)
+        }
+
+    }
+    func refresh_my(){
+        myTableView.mj_header.endRefreshing()
     }
 }
